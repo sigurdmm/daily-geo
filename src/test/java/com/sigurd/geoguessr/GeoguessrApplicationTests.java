72e8c027517c03b1f273b8b561a167548fa1d72d
Sigurd Melsom <sigurd.m.melsom@gmail.com>
2023-12-08 21:10:05 +0100

    Test setup

--- src/test/java/com/sigurd/geoguessr/GeoguessrApplicationTests.java
+++ src/test/java/com/sigurd/geoguessr/GeoguessrApplicationTests.java
@@ -2,10 +2,11 @@ package com.sigurd.geoguessr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "/application.properties")
 class GeoguessrApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
